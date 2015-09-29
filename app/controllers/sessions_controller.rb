0d526fe61307e1a6c654ce30a0674f047f8ba892
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-29 13:39:47 -0600

    Current User method, Users/Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Invalid Login"
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = "Logout Successful"
+    redirect_to root_path
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Your account was successfully created!"
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Please be sure to include a name and a valid email."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
+  end
+end
